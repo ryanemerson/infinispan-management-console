4bae6a43d78aa9c38322d2a25f22abb73a039e63
Ryan Emerson <remerson@redhat.com>
2016-08-08 15:33:34 +0100

    Loader Directive changed to a component.
    Everything works as expected, with one exception:
    - $scope.$emit no longer works as this is contradictory to the in/out relationship of the component model.
    - Instead, the parent controller should pass a function callback to the component it's calling:
       - E.g. edit-cache-controller should pass the function that defines the changedFields behaviour to the component in
         the edit-cache.html template. Sub components should then also pass this method.

--- src/main/webapp/components/directives/cache/loaders/cache-loader.js
+++ src/main/webapp/components/directives/cache/loaders/cache-loader.js
@@ -3,208 +3,204 @@
 
   var module = angular.module('ispn.directives.cache.cacheLoader', ['ispn.services.utils']);
 
-  module.directive('cacheLoader', ['utils', function (utils) {
-    return {
-      restrict: 'E',
-      scope: {
-        data: '=',
-        metadata: '=',
-        readOnly: '=',
-        outsideController: '='
-      },
-      replace: true,
-      templateUrl: 'components/directives/cache/loaders/cache-loader.html',
-      link: function (scope) {
+  module.component('cacheLoader', {
+    bindings: {
+      data: '=',
+      metadata: '=',
+      readOnly: '=',
+      outsideController: '='
+    },
+    templateUrl: 'components/directives/cache/loaders/cache-loader.html',
+    controller: function (utils) {
+      var vm = this;
 
-        scope.init = function () {
-          scope.fields = ['preload', 'shared'];
-          scope.allFields = scope.fields.concat(scope.fields, 'class');
-          scope.cacheLoaders = [
-            {
-              class: 'None',
-              label: 'No Cache Loader'
-            },
-            {
-              class: 'org.infinispan.persistence.async.AsyncCacheLoader',
-              label: 'Async Cache Loader'
-            },
-            {
-              class: 'org.infinispan.persistence.async.AdvancedAsyncCacheLoader',
-              label: 'Advanced Async Cache Loader'
-            },
-            {
-              class: 'org.infinispan.persistence.jdbc.binary.JdbcBinaryStore',
-              label: 'Binary Based JDBC Store'
-            },
-            {
-              class: 'org.infinispan.persistence.cluster.ClusterLoader',
-              label: 'Cluster Loader'
-            },
-            {
-              class: '',
-              label: 'Custom Loader'
-            },
-            {
-              class: 'org.infinispan.persistence.jpa.JpaStore',
-              label: 'JPA Store'
-            },
-            {
-              class: 'org.infinispan.persistence.leveldb.LevelDBStore',
-              label: 'LevelDB Store'
-            },
-            {
-              class: 'org.infinispan.persistence.jdbc.mixed.JdbcMixedStore',
-              label: 'Mixed JDBC Store'
-            },
-            {
-              class: 'org.infinispan.persistence.remote.RemoteStore',
-              label: 'Remote Store'
-            },
-            {
-              class: 'org.infinispan.persistence.rest.RestStore',
-              label: 'Rest Store'
-            },
-            {
-              class: 'org.infinispan.persistence.file.SingleFileStore',
-              label: 'Single File Store'
-            },
-            {
-              class: 'org.infinispan.persistence.sifs.SoftIndexFileStore',
-              label: 'Soft Index File Store'
-            },
-            {
-              class: 'org.infinispan.persistence.jdbc.stringbased.JdbcStringBasedStore',
-              label: 'String Based JDBC Store'
-            }
-          ];
-          scope.initInternalController();
-          scope.initPrevDataAndDefaults();
-          scope.cleanMetadata();
-        };
+      vm.init = function () {
+        vm.fields = ['preload', 'shared'];
+        vm.allFields = vm.fields.concat(vm.fields, 'class');
+        vm.cacheLoaders = [
+          {
+            class: 'None',
+            label: 'No Cache Loader'
+          },
+          {
+            class: 'org.infinispan.persistence.async.AsyncCacheLoader',
+            label: 'Async Cache Loader'
+          },
+          {
+            class: 'org.infinispan.persistence.async.AdvancedAsyncCacheLoader',
+            label: 'Advanced Async Cache Loader'
+          },
+          {
+            class: 'org.infinispan.persistence.jdbc.binary.JdbcBinaryStore',
+            label: 'Binary Based JDBC Store'
+          },
+          {
+            class: 'org.infinispan.persistence.cluster.ClusterLoader',
+            label: 'Cluster Loader'
+          },
+          {
+            class: '',
+            label: 'Custom Loader'
+          },
+          {
+            class: 'org.infinispan.persistence.jpa.JpaStore',
+            label: 'JPA Store'
+          },
+          {
+            class: 'org.infinispan.persistence.leveldb.LevelDBStore',
+            label: 'LevelDB Store'
+          },
+          {
+            class: 'org.infinispan.persistence.jdbc.mixed.JdbcMixedStore',
+            label: 'Mixed JDBC Store'
+          },
+          {
+            class: 'org.infinispan.persistence.remote.RemoteStore',
+            label: 'Remote Store'
+          },
+          {
+            class: 'org.infinispan.persistence.rest.RestStore',
+            label: 'Rest Store'
+          },
+          {
+            class: 'org.infinispan.persistence.file.SingleFileStore',
+            label: 'Single File Store'
+          },
+          {
+            class: 'org.infinispan.persistence.sifs.SoftIndexFileStore',
+            label: 'Soft Index File Store'
+          },
+          {
+            class: 'org.infinispan.persistence.jdbc.stringbased.JdbcStringBasedStore',
+            label: 'String Based JDBC Store'
+          }
+        ];
+        vm.initInternalController();
+        vm.initPrevDataAndDefaults();
+        vm.cleanMetadata();
+      };
 
-        scope.initInternalController = function () {
-          if (utils.isNotNullOrUndefined(scope.outsideController)) {
-            if (utils.isArray(scope.outsideController)) {
-              var handle = {};
-              scope.outsideController.push(handle);
-              scope.internalController = handle;
-            } else {
-              scope.internalController = scope.outsideController;
-            }
+      vm.initInternalController = function () {
+        if (utils.isNotNullOrUndefined(vm.outsideController)) {
+          if (utils.isArray(vm.outsideController)) {
+            var handle = {};
+            vm.outsideController.push(handle);
+            vm.internalController = handle;
           } else {
-            scope.internalController = {};
+            vm.internalController = vm.outsideController;
           }
+        } else {
+          vm.internalController = {};
+        }
 
-          scope.internalController.requiresRestart = scope.requiresRestart;
-          scope.internalController.cleanMetadata = scope.cleanMetadata;
-          scope.internalController.isAnyFieldModified = scope.isAnyFieldModified;
-        };
+        vm.internalController.requiresRestart = vm.requiresRestart;
+        vm.internalController.cleanMetadata = vm.cleanMetadata;
+        vm.internalController.isAnyFieldModified = vm.isAnyFieldModified;
+      };
 
-        scope.initPrevDataAndDefaults = function () {
-          scope.prevData = {};
-          //if not initializing to defaults then make root node in the model tree (if not existing already)
-          if (!utils.isNotNullOrUndefined(scope.data)) {
-            scope.data = {};
-          }
+      vm.initPrevDataAndDefaults = function () {
+        vm.prevData = {};
+        //if not initializing to defaults then make root node in the model tree (if not existing already)
+        if (!utils.isNotNullOrUndefined(vm.data)) {
+          vm.data = {};
+        }
 
-          scope.data['is-new-node'] = utils.isNullOrUndefined(scope.data['class']);
-          scope.type = {
-            type: scope.getStoreType(scope.data.class, scope.cacheLoaders)
-          }
-        };
+        vm.data['is-new-node'] = utils.isNullOrUndefined(vm.data['class']);
+        vm.type = {
+          type: vm.getStoreType(vm.data.class, vm.cacheLoaders)
+        }
+      };
 
-        scope.cleanFieldMetadata = function (field) {
-          if (utils.isNotNullOrUndefined(scope.metadata[field])) {
-            utils.makeFieldClean(scope.metadata[field]);
+      vm.cleanFieldMetadata = function (field) {
+        if (utils.isNotNullOrUndefined(vm.metadata[field])) {
+          utils.makeFieldClean(vm.metadata[field]);
+        } else {
+          console.log("Cleaning metadata for configuration field " + field + ", that field does not exist in DMR model")
+        }
+      };
+
+      vm.cleanMetadata = function () {
+        vm.allFields.forEach(function (attrName) {
+          vm.cleanFieldMetadata(attrName);
+          if (utils.isNotNullOrUndefined(vm.data[attrName])) {
+            vm.prevData[attrName] = angular.copy(vm.data[attrName]);
           } else {
-            console.log("Cleaning metadata for configuration field " + field + ", that field does not exist in DMR model")
+            vm.prevData[attrName] = '';
           }
-        };
+        });
+        vm.prevData['type'] = vm.type.type;
+        vm.cleanFieldMetadata('class');
+      };
 
-        scope.cleanMetadata = function () {
-          scope.allFields.forEach(function (attrName) {
-            scope.cleanFieldMetadata(attrName);
-            if (utils.isNotNullOrUndefined(scope.data[attrName])) {
-              scope.prevData[attrName] = angular.copy(scope.data[attrName]);
-            } else {
-              scope.prevData[attrName] = '';
-            }
+      vm.undoFieldChange = function (field) {
+        vm.data[field] = vm.prevData[field];
+        utils.makeFieldClean(vm.metadata[field]);
+      };
+
+      vm.resolveFieldType = function (field) {
+        return utils.resolveFieldType(vm.metadata, field);
+      };
+
+      vm.isAnyFieldModified = function () {
+        return vm.allFields.some(function (attrName) {
+          return utils.isFieldValueModified(vm.metadata[attrName]);
+        });
+      };
+
+      vm.requiresRestart = function () {
+        return vm.allFields.some(function (attrName) {
+          return utils.isFieldValueModified(vm.metadata[attrName]) && (utils.fieldChangeRequiresRestart(vm.metadata[attrName]));
+        });
+      };
+
+      vm.fieldValueModified = function (field) {
+        var meta = vm.metadata[field];
+        var original = vm.prevData[field];
+        var latest = vm.data[field];
+
+        if (((utils.isNullOrUndefined(original) || original === '') && !latest) || original === latest) {
+          utils.makeFieldClean(meta, field, true, vm);
+        } else {
+          utils.makeFieldDirty(meta, field, true, vm);
+        }
+      };
+
+      vm.changeLoaderClass = function () {
+        vm.data['class'] = utils.isNullOrUndefined(vm.type.type) ? null : angular.copy(vm.type.type);
+        vm.fieldValueModified('class');
+      };
+
+      vm.undoClassChange = function () {
+        vm.undoFieldChange('class');
+        vm.type.type = vm.getStoreType(vm.data['class'], vm.cacheLoaders);
+      };
+
+      vm.getStyle = function (field) {
+        return utils.isNotNullOrUndefined(vm.metadata[field]) ? vm.metadata[field].style : '';
+      };
+
+      vm.getStoreType = function (classValue, cacheLoaders) {
+        if (utils.isNullOrUndefined(classValue)) {
+          return 'None';
+        } else {
+          var nonCustomStore = cacheLoaders.some(function (loader) {
+            return loader.class === classValue;
           });
-          scope.prevData['type'] = scope.type.type;
-          scope.cleanFieldMetadata('class');
-        };
+          return nonCustomStore ? classValue : '';
+        }
+      };
 
-        scope.undoFieldChange = function (field) {
-          scope.data[field] = scope.prevData[field];
-          utils.makeFieldClean(scope.metadata[field]);
-        };
-
-        scope.resolveFieldType = function (field) {
-          return utils.resolveFieldType(scope.metadata, field);
-        };
-
-        scope.isAnyFieldModified = function () {
-          return scope.allFields.some(function (attrName) {
-            return utils.isFieldValueModified(scope.metadata[attrName]);
+      vm.isCustomLoader = function () {
+        if (utils.isNotNullOrUndefined(vm.type.type) && vm.type.type.length == 0) {
+          return true;
+        } else {
+          return !vm.cacheLoaders.some(function (loader) {
+            return loader.class !== vm.data.class;
           });
-        };
+        }
+      };
 
-        scope.requiresRestart = function () {
-          return scope.allFields.some(function (attrName) {
-            return utils.isFieldValueModified(scope.metadata[attrName]) && (utils.fieldChangeRequiresRestart(scope.metadata[attrName]));
-          });
-        };
-
-        scope.fieldValueModified = function (field) {
-          var meta= scope.metadata[field];
-          var original = scope.prevData[field];
-          var latest = scope.data[field];
-
-          if (((utils.isNullOrUndefined(original) || original === '') && !latest) || original === latest) {
-            utils.makeFieldClean(meta, field, true, scope);
-          } else {
-            utils.makeFieldDirty(meta, field, true, scope);
-          }
-        };
-
-        scope.changeLoaderClass = function () {
-          scope.data['class'] = utils.isNullOrUndefined(scope.type.type) ? null : angular.copy(scope.type.type);
-          scope.fieldValueModified('class');
-        };
-
-        scope.undoClassChange = function () {
-          scope.undoFieldChange('class');
-          scope.type.type = scope.getStoreType(scope.data['class'], scope.cacheLoaders);
-        };
-
-        scope.getStyle = function (field) {
-          return utils.isNotNullOrUndefined(scope.metadata[field]) ? scope.metadata[field].style : '';
-        };
-
-        scope.getStoreType = function (classValue, cacheLoaders) {
-          if (utils.isNullOrUndefined(classValue)) {
-            return 'None';
-          } else {
-            var nonCustomStore = cacheLoaders.some(function (loader) {
-              return loader.class === classValue;
-            });
-            return nonCustomStore ? classValue : '';
-          }
-        };
-
-        scope.isCustomLoader = function () {
-          if (utils.isNotNullOrUndefined(scope.type.type) && scope.type.type.length == 0) {
-            return true;
-          } else {
-            return !scope.cacheLoaders.some(function (loader) {
-              return loader.class !== scope.data.class;
-            });
-          }
-        };
-
-        scope.init();
-      }
-    };
-  }
-  ]);
+      vm.init();
+    }
+  });
 }());
