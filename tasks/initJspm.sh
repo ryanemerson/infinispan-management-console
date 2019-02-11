35ba0dacd10fb4804c5257ff5cc03d6a110d8102
Ryan Emerson <remerson@redhat.com>
2019-02-11 10:44:26 +0000

    Add jspm-loader-files to cache

--- tasks/initJspm.sh
+++ tasks/initJspm.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 set -o xtrace
-set -e
 
 ROOT=$PWD
+JSPM_LOADER_FILES=$ROOT/jspm-loader-files
 JSPM_MODS=$ROOT/jspm-modules
 JSPM_REG=$ROOT/jspm-registry
 CACHE_DIR=~/.jspm/packages/github/jspm/
+LOADER_DIR=~/.jspm/loader-files/
 
 mkdir -p $CACHE_DIR
+mkdir -p $LOADER_DIR
 cp -R $JSPM_MODS/* $CACHE_DIR
+cp -R $JSPM_LOADER_FILES/* $LOADER_DIR
 ls -l $CACHE_DIR
+ls -l $LOADER_DIR
 
 cd $JSPM_REG
 rm -rf .git
@@ -23,4 +27,6 @@ cd $ROOT
 jspm config registries.jspm.repo $ROOT/jspm-registry/.git
 jspm setmode local
 jspm install
+
+ls -l ~/.jspm/loader-files/systemjs
 
